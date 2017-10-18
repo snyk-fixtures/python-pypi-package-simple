e5820c3a0f19330375eb45e04b68544c24fb6c4d
Shaun Smith <shaun@snyk.io>
2017-10-18 17:30:16 +0100

    feat: init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simple',
+      version='0.1',
+      description='Simple pypi package',
+      url='https://github.com/snyk-fixtures/python-pypi-package-simple',
+      author='anon',
+      author_email='anon@example.com',
+      license='MIT',
+      packages=['simple'],
+      zip_safe=False)
